a3a417e6fccc4a7a5b3b68ca005e433318ad92f8
Farhan284 <azka58865@gail.com>
2025-03-13 10:00:51 +0700

    loan

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;  // Pastikan ini menggunakan huruf kapital pada "C"
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoansController;
 
 // Menggunakan apiResource untuk resource CRUD standar
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('books', BookController::class);
 Route::apiResource('users', UserController::class);
+Route::apiResource('loans', LoansController::class);
 
 // Route contoh untuk mengambil user yang sudah autentikasi menggunakan Sanctum
